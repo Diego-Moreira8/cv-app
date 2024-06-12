13615b710ca7762a3dec2a5bb7a6f2e348be5490
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2024-06-12 20:56:06 -0300

    feat: personal data inputs

--- src/components/CVForms.tsx
+++ src/components/CVForms.tsx
@@ -4,18 +4,36 @@ import PersonalDataForm from "./PersonalDataForm";
 
 type PersonalData = {
   name: string;
+  location: string;
+  phone: string;
+  email: string;
 };
 
 export type CVData = {
   personalData: PersonalData;
 };
 
-export type CVAction = {
-  type: "SET_NAME";
-  value: string;
-};
+export type CVAction =
+  | {
+      type: "SET_NAME";
+      value: string;
+    }
+  | {
+      type: "SET_LOCATION";
+      value: string;
+    }
+  | {
+      type: "SET_PHONE";
+      value: string;
+    }
+  | {
+      type: "SET_EMAIL";
+      value: string;
+    };
 
-const INITIAL_CV: CVData = { personalData: { name: "" } };
+const INITIAL_CV: CVData = {
+  personalData: { name: "", location: "", phone: "", email: "" },
+};
 
 function cvReducer(state: CVData, action: CVAction) {
   switch (action.type) {
@@ -23,6 +41,21 @@ function cvReducer(state: CVData, action: CVAction) {
       return {
         ...state,
         personalData: { ...state.personalData, name: action.value },
+      };
+    case "SET_LOCATION":
+      return {
+        ...state,
+        personalData: { ...state.personalData, location: action.value },
+      };
+    case "SET_PHONE":
+      return {
+        ...state,
+        personalData: { ...state.personalData, phone: action.value },
+      };
+    case "SET_EMAIL":
+      return {
+        ...state,
+        personalData: { ...state.personalData, email: action.value },
       };
   }
 }
--- src/components/PersonalDataForm.tsx
+++ src/components/PersonalDataForm.tsx
@@ -11,24 +11,76 @@ export default function PersonalDataForm({
   cvState,
   cvDispatch,
 }: PersonalDataFormProps) {
-  const { name } = cvState.personalData;
+  const { name, location, phone, email } = cvState.personalData;
 
-  const fullNameId = useId();
+  const fullNameInputId = useId();
+  const locationInputId = useId();
+  const phoneInputId = useId();
+  const emailInputId = useId();
 
   function handleFullNameChange(e: React.ChangeEvent<HTMLInputElement>) {
     cvDispatch({ type: "SET_NAME", value: e.target.value });
   }
 
+  function handleLocationChange(e: React.ChangeEvent<HTMLInputElement>) {
+    cvDispatch({ type: "SET_LOCATION", value: e.target.value });
+  }
+
+  function handlePhoneChange(e: React.ChangeEvent<HTMLInputElement>) {
+    cvDispatch({ type: "SET_PHONE", value: e.target.value });
+  }
+
+  function handleEmailChange(e: React.ChangeEvent<HTMLInputElement>) {
+    cvDispatch({ type: "SET_EMAIL", value: e.target.value });
+  }
+
   return (
     <Group title="Dados pessoais">
       <div>
-        <label htmlFor={fullNameId}>Nome completo:</label>
+        <label htmlFor={fullNameInputId}>Nome completo:</label>
         <input
           type="text"
           name="fullName"
-          id={fullNameId}
+          id={fullNameInputId}
+          placeholder="João Silva"
           value={name}
           onChange={handleFullNameChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor={locationInputId}>Localização:</label>
+        <input
+          type="text"
+          name="location"
+          id={locationInputId}
+          placeholder="São Paulo - SP"
+          value={location}
+          onChange={handleLocationChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor={phoneInputId}>Celular:</label>
+        <input
+          type="text"
+          name="phone"
+          id={phoneInputId}
+          placeholder="(11) 98765-4321"
+          value={phone}
+          onChange={handlePhoneChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor={emailInputId}>E-mail:</label>
+        <input
+          type="email"
+          name="email"
+          id={emailInputId}
+          placeholder="joaosilva@gmail.com"
+          value={email}
+          onChange={handleEmailChange}
         />
       </div>
     </Group>
