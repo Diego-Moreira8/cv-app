d6a1ee60605340cffe628922b1d7a0854af09580
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2024-06-16 16:29:20 -0300

    feat: exp list styles

--- src/components/ExpList.tsx
+++ src/components/ExpList.tsx
@@ -1,6 +1,7 @@
 import { CVData, CVAction, ExpType } from "../useCVReducer";
 import months from "../utils/monthsArray";
 import { ExpGroupActions } from "./ExpGroup";
+import styles from "../styles/ExpList.module.css";
 
 type ExpListProps = {
   expType: ExpType;
@@ -33,32 +34,42 @@ export default function ExpList({
   }
 
   return (
-    <div>
-      <button type="button" onClick={handleAdd}>
-        Adicionar experiência
+    <div className={styles.expList}>
+      <button className="ok" type="button" onClick={handleAdd}>
+        Adicionar
       </button>
+
+      {experiences.length === 0 && (
+        <p className={styles.emptyList}>
+          <i>Suas experiências aparecerão aqui</i>
+        </p>
+      )}
 
       <ul>
         {experiences.map((exp) => (
           <li key={exp.id}>
-            <p>
-              <strong>{exp.title}</strong>
-            </p>
+            <hr />
+
+            <h3>{exp.title}</h3>
 
             <p>{exp.location}</p>
 
             <p>
-              de {months[exp.startMonth]}/{exp.startYear} até{" "}
-              {months[exp.endMonth]}/{exp.endYear}
+              De {months[exp.startMonth - 1].toLowerCase()}/{exp.startYear} até{" "}
+              {months[exp.endMonth - 1].toLowerCase()}/{exp.endYear}
             </p>
 
             <p>{exp.description}</p>
 
-            <div>
+            <div className={styles.buttons}>
               <button type="button" onClick={() => handleEdit(exp.id)}>
                 Editar
               </button>
-              <button type="button" onClick={() => handleDelete(exp.id)}>
+              <button
+                className="danger"
+                type="button"
+                onClick={() => handleDelete(exp.id)}
+              >
                 Apagar
               </button>
             </div>
