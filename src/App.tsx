f45c0042619d5f868399c3d77272c2ec0685d3cb
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2024-06-25 18:52:18 -0300

    fix: document being cropped when printing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
 import { Header } from "./components/Header";
 import { CVForms } from "./components/CVForms";
 import { Footer } from "./components/Footer";
 import styles from "./styles/App.module.css";
 
 function App() {
+  useEffect(() => {
+    // Prevents the document to be cropped when printing
+    const handleBeforePrint = () => window.scrollTo({ top: 0 });
+    window.addEventListener("beforeprint", handleBeforePrint);
+    return () => window.removeEventListener("beforeprint", handleBeforePrint);
+  }, []);
+
   return (
     <div className={styles.app} id="app">
       <Header />
