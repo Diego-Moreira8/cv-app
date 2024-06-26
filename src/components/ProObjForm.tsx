bb5df1d6331323a2298e0f117f8190eb9a0a7c2f
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2024-06-26 18:19:58 -0300

    feat: pro. obj. form options

--- src/components/ProObjForm.tsx
+++ src/components/ProObjForm.tsx
@@ -2,10 +2,20 @@ import { Group } from "./Group";
 import { FormWrapper } from "./FromWrapper";
 import { Textarea } from "./Textarea";
 import { useCVState, useCVDispatch } from "../cv-reducer/hook";
+import { PRO_OBJ_EXAMPLE } from "../cv-reducer/cvTemplates";
+import styles from "../styles/ProObjForm.module.css";
 
 function ProObjForm() {
   const cvState = useCVState();
   const cvDispatch = useCVDispatch();
+
+  function addTemplate() {
+    cvDispatch({ type: "SET_PROFESSIONAL_OBJECTIVE", value: PRO_OBJ_EXAMPLE });
+  }
+
+  function clear() {
+    cvDispatch({ type: "SET_PROFESSIONAL_OBJECTIVE", value: "" });
+  }
 
   return (
     <Group title="Objetivo profissional">
@@ -22,6 +32,16 @@ function ProObjForm() {
             })
           }
         />
+
+        <div className={styles.buttons}>
+          <button type="button" onClick={addTemplate}>
+            Inserir um modelo
+          </button>
+
+          <button className="danger" type="button" onClick={clear}>
+            Limpar
+          </button>
+        </div>
       </FormWrapper>
     </Group>
   );
