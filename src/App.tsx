b2f417638cd04194d768f8ad97becb79516784be
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2024-06-28 10:13:05 -0300

    feat: dynamic title

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { useEffect } from "react";
 import { Header } from "./components/Header";
 import { CVForms } from "./components/CVForms";
-import { Footer } from "./components/Footer";
-import styles from "./styles/App.module.css";
 import { PrintButton } from "./components/PrintButton";
+import { Footer } from "./components/Footer";
+import { useCVState } from "./cv-reducer/hook";
+import styles from "./styles/App.module.css";
 
 function App() {
+  const {
+    personalData: { name },
+  } = useCVState();
+
+  useEffect(() => {
+    document.title = `Currículo${name && " - " + name}`;
+  }, [name]);
+
   useEffect(() => {
     // Prevents the document to be cropped when printing
     const handleBeforePrint = () => window.scrollTo({ top: 0 });
